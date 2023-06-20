35cb1adf55ee044e34934431321d2119a9aff007
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-06-20 20:25:41 +0300

    feat: add greeting current path logic

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { argv, stdin, stdout } from 'process';
 import readline from 'readline';
+import os from 'os';
 
+let currentPath = os.homedir();
 const usernameArgv = argv.find((arg) => arg.startsWith('--username'));
 const username = usernameArgv ? usernameArgv.slice(11) : 'Noname';
 stdout.write(`Welcome to the File Manager, ${username}!\n`);
+stdout.write(`You are currently in ${currentPath}\n`);
 
 const rl = readline.createInterface({ input: stdin, output: stdout });
 
@@ -11,6 +14,7 @@ rl.on('line', (input) => {
   if (input === '.exit') {
     rl.close();
   }
+  stdout.write(`You are currently in ${currentPath}\n`);
 });
 
 rl.on('close', () => {
